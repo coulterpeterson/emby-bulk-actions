5b47477a39ce552969937b436140e3716902593b
Coulter Peterson <coulterpeterson@users.noreply.github.com>
2023-10-16 12:30:39 -0400

    Initial commit

---
+++ disable_unmapped_channels.py
@@ -0,0 +1,79 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SERVER_URL = os.getenv('SERVER_URL')
+API_KEY = os.getenv('API_KEY')
+CHANNEL_EXCLUSION_SEARCH_TERMS = os.getenv('CHANNEL_EXCLUSION_SEARCH_TERMS')
+
+channel_loop_index = 0
+
+# Get all unmapped channels
+get_unmapped_channels_query = {
+  'SortBy':'DefaultChannelOrder', 
+  'SortOrder':'Descending',
+  'Fields':'BasicSyncInfo%2CCanDelete%2CContainer%2CPrimaryImageAspectRatio%2CProductionYear%2CStatus%2CEndDate%2CCommunityRating%2COfficialRating%2CCriticRating',
+  'StartIndex':channel_loop_index,
+  'EnableImageTypes':'Primary%2CBackdrop%2CThumb',
+  'ImageTypeLimit':'1',
+  'ChannelMappingStatus':'Unmapped',
+  'Limit':'30',
+  'X-Emby-Token':API_KEY,
+  'X-Emby-Language':'en-us',
+  }
+
+get_unmapped_channels_response = requests.get(SERVER_URL + "/emby/LiveTv/Manage/Channels", params=get_unmapped_channels_query)
+
+unmapped_channels = get_unmapped_channels_response.json()
+
+# Example of Single Channel's Data:
+# {'Name': 'US| MAGNOLIA CHANNEL HD', 
+#  'ServerId': 'fe12a368204e41348dc76fbcf9baf2a8', 
+#  'Id': '699973', 
+#  'SortIndexNumber': 25, 
+#  'ChannelNumber': '254715', 
+#  'Type': 'ChannelManagementInfo', 
+#  'ImageTags': {'Primary': '6a1e338c9f7458bf3ac44712085a376a'}, 
+#  'BackdropImageTags': [], 
+#  'Disabled': False, 
+#  'ManagementId': '4ab97cfd1764498fb930c78539b9fb29_m3u_72d5a993df2923b222644d4ad055215b81659f8e21582271b85b15899614e094'
+#  }
+
+i = 0
+# Now that we have the initial count of unmapped channels, start the loop
+while len(unmapped_channels['Items']) > 0:
+
+  # Disable channels
+  for channel in unmapped_channels['Items']:
+      i += 1
+      name = channel['Name']
+      id = channel['Id']
+      management_id = channel['ManagementId']
+      disabled = channel['Disabled']
+      exclusion_term_detected = False
+      for term in CHANNEL_EXCLUSION_SEARCH_TERMS:
+          if term in name:
+              exclusion_term_detected = True
+      if not disabled and not exclusion_term_detected:
+        disable_channel_query = {
+          'ManagementId':management_id, 
+          'Disabled':'true',
+          'X-Emby-Token':API_KEY,
+          'X-Emby-Language':'en-us',
+          'reqformat':'json',
+          }
+        disable_channel = requests.post(SERVER_URL + '/emby/LiveTv/Manage/Channels/'+ id +'/Disabled',params=disable_channel_query)
+        print(str(i) + " Disabled Channel: " + name + " (" + id + ")")
+      else:
+        print(str(i) + " Skipped Channel: " + name + " (" + id + ")")
+  # END channel in unmapped_channels['Items']:
+
+  # Increment the channel_loop_index and get another batch of unmapped channels
+  channel_loop_index += 30
+  get_unmapped_channels_response = requests.get(SERVER_URL + "/emby/LiveTv/Manage/Channels", params=get_unmapped_channels_query)
+  unmapped_channels = get_unmapped_channels_response.json()
+
+# END while len(unmapped_channels['Items']) > 0:
+
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Emby Bulk Edits
+
+## Installation (Windows)
+* `choco install python`
+* `python -m pip install -U pip`
+* `pip install requests`
+* `pip install -U python-dotenv`
+* Clone repo
+* Copy `.env_example` to `.env` and update values
+
+## Running Disable Unmapped Channels
+* `python disable_unmapped_channels.py`
+* CTRL+C to cancel at any time
